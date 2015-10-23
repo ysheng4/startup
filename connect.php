5f22db77311f22a572b212e1f442ff07bc898772
ysheng4 <aegeanjie@gmail.com>
2015-10-22 23:10:23 -0500

    v-1

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+$db_host="localhost";
+$db_user="root";
+$db_password="";
+$db_database='test-1';
+
+$connect=mysqli_connect($db_host,$db_user,$db_password,$db_database) or die('Connect error');
+
+
+
+?>
